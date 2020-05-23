1317978c33fb76141411d18edc0b07f4a5786415
harish988 <harishharish988@gmail.com>
2020-05-23 20:00:37 +0530

    Added deployment files

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn quiz.py --log-file -
 web: gunicorn quiz.wsgi:application --log-file - --log-level debug
--- question/models.py
+++ question/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 #Domain Model
 class Domain(models.Model):
@@ -24,7 +25,7 @@ class Quiz(models.Model):
     pass_mark = models.IntegerField()
     total_marks = models.IntegerField()
     time_in_minutes = models.IntegerField()
-    quiz_image = models.BinaryField(blank=True)
+    quiz_image = models.ImageField(upload_to="quiz/staticfiles", null=True)
     hardness_choices = (
         ('BEGINNER', 'Beginner'),
         ('INTERMEDIATE', 'Intermediate'),
@@ -34,6 +35,13 @@ class Quiz(models.Model):
 
     def __str__(self):
         return self.name
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image.url)
+        else:
+            return 'No Image Found'
+    image_tag.short_description = 'Image'
 
     class Meta:
         verbose_name_plural = "Quizzes"
@@ -45,7 +53,7 @@ class Question(models.Model):
     id = models.AutoField(primary_key=True)
     quiz_id = models.ForeignKey(Quiz, verbose_name="quiz", on_delete=models.CASCADE)
     question_text = models.CharField(max_length=5000)
-    question_image = models.BinaryField(blank=True)
+    question_image = models.ImageField(upload_to="quiz/staticfiles", null=True)
     mark = models.IntegerField()
     question_type_choices = (
         ('SINGLECHOICE', 'SingleChoice'),
@@ -58,6 +66,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def image_tag(self):
+        if self.image:
+            return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image.url)
+        else:
+            return 'No Image Found'
+    image_tag.short_description = 'Image'
+
     class Meta:
         verbose_name_plural = "Questions"
         managed = True
@@ -69,7 +84,7 @@ class Answer(models.Model):
     quiz_id = models.ForeignKey(Quiz, verbose_name="quiz", on_delete=models.CASCADE)
     question_id = models.ForeignKey(Question, verbose_name="question", on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=500)
-    answer_image = models.BinaryField(blank=True)
+    answer_image = models.ImageField(upload_to="quiz/staticfiles", null=True)
     is_correct_answer_choices = (
         ('TRUE', 'True'),
         ('FALSE', 'False'),
@@ -78,6 +93,13 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.answer_text
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image.url)
+        else:
+            return 'No Image Found'
+    image_tag.short_description = 'Image'
 
     class Meta:
         verbose_name_plural = "Answers"
