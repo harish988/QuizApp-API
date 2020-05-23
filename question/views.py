ed17cf6f425dcaf52fbc277c78fc56a621d31c4e
harish988 <harishharish988@gmail.com>
2020-05-23 07:49:56 +0530

    Mofdified answer API and published leaderboard API

--- question/views.py
+++ question/views.py
@@ -1,20 +1,36 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Quiz, Question, Answer
+from score.models import UserAnswer, UserScore
+from user.models import UserProfile
+from rest_framework import status
+import json
+from rest_framework.decorators import api_view
 
-def quiz(request):
+
+@api_view(['GET'])
+def quiz(request, user_id):
     if(request.method == 'GET'):
         quizzes = Quiz.objects.all()
         response = {}
         body = list()
         for quiz in quizzes:
-            s = {'id': quiz.id, 'name': quiz.name, 'description': quiz.description, 'domain_name': quiz.domain_id.name, 'no_of_questions': quiz.no_of_questions, 'no_of_answers_to_display': quiz.no_of_question_to_display, 'pass_mark': quiz.pass_mark, 'time': quiz.time_in_minutes, 'hardness': quiz.hardness}
+            scores_present = UserScore.objects.filter(user=user_id, quiz_id=quiz.id).count() > 0
+            resume_quiz = UserAnswer.objects.filter(user=user_id, quiz_id=quiz.id).count() < quiz.no_of_question_to_display
+            s = {'id': quiz.id, 'name': quiz.name, 'description': quiz.description, 'domain_name': quiz.domain_id.name, 'no_of_questions': quiz.no_of_questions, 'no_of_answers_to_display': quiz.no_of_question_to_display, 'pass_mark': quiz.pass_mark, 'time': quiz.time_in_minutes, 'hardness': quiz.hardness, "attended": scores_present, "resume": resume_quiz}
             body.append(s)
         response['quiz'] = body
-        return JsonResponse(response)
+        return JsonResponse(response, status=status.HTTP_200_OK)
 
-def questions_with_answers(request, quiz_id, question_id=None):
+
+@api_view(['GET'])
+def questions_with_answers(request, user_id, quiz_id, question_id=None):
     if(request.method == 'GET'):
+        answered_questions = None
+        answered_questions_ids = set()
+        answered_questions = UserAnswer.objects.filter(user=user_id, quiz_id=quiz_id)
+        for answered_question in answered_questions:
+            answered_questions_ids.add(answered_question.question_id)
         if(question_id is None):
             answers = Answer.objects.filter(quiz_id = quiz_id)
         else:
@@ -30,13 +46,19 @@ def questions_with_answers(request, quiz_id, question_id=None):
                 before_adding_answers.append(ans)
                 question_answers[answer.question_id] = before_adding_answers
             else:
+                if(answered_questions_ids is not None and answer.question_id in answered_questions_ids):
+                    continue
                 ans = {'id': answer.id, 'answer_text': answer.answer_text, 'is_correct': answer.is_correct_answer}
                 question_answers[answer.question_id] = [ans]
                 question_properties[answer.question_id] = {'id': answer.question_id.id, 'question_text': answer.question_id.question_text, 'mark': answer.question_id.mark, 'question_type': answer.question_id.question_type, 'no_of_answers': answer.question_id.no_of_answers}
                 question_ids.append(answer.question_id)
         questions = list()
+        answered_questions_count = 0
+        if(len(answered_questions_ids) > 0):
+            answered_questions_count = len(answered_questions_ids)
+        response['answered_questions_count'] = answered_questions_count
         for id in question_ids:
             question_properties[id]['answers'] = question_answers[id]
             questions.append(question_properties[id])
         response['question'] = questions
-        return JsonResponse(response)
+        return JsonResponse(response, status=status.HTTP_200_OK)
--- user/views.py
+++ user/views.py
@@ -6,8 +6,11 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 import datetime
 import traceback
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 @csrf_exempt
+@api_view(["POST"])
 def user(request):
     if(request.method == 'POST'):
         json_data = json.loads(request.body)
@@ -16,7 +19,7 @@ def user(request):
         body = list()
         try:
             response["details"] = {}
-            response["status"] = 400
+            response["status"] = 404
             response["code"] = "FAILED"
             for data in user:
                 user = User.objects.create_user(username=data["roll_no"], email=data["email"], password=data["password"], first_name=data["first_name"], last_name=data["last_name"])
@@ -47,7 +50,41 @@ def user(request):
             response["status"] = 400
             response["code"] = "FAILED"
             response["mesaage"] = traceback.print_exc()
-            return JsonResponse(response)
+            return JsonResponse(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    if(request.method == 'GET'):
+        users = None
+        response = {}
+        users = UserProfile.objects.all()
+        if(users.count == 0):
+            response["message"] = "User Not Present"
+            response["details"] = {}
+            response["status"] = 404
+            response["code"] = "FAILED"
+            return JsonResponse(response, status=status.HTTP_404_RESOURCE_NOT_FOUND)
+        body = list()
+        for user in users:
+            body.append({'id': user.id, "first_name": user.user.first_name, "last_name": user.user.last_name, "email": user.user.email, "department": user.department.name, "year": user.year.name, "section": user.section.name})
+        response["user"] = body
+        return JsonResponse(response, status=status.HTTP_200_OK)
+
+@api_view(["GET"])
+def userById(request, user_id):
+    if(request.method == 'GET'):
+        users = None
+        response = {}
+        users = UserProfile.objects.filter(id=user_id)
+        if(users.count() == 0):
+            response["message"] = "User Not Present"
+            response["details"] = {}
+            response["status"] = 404
+            response["code"] = "FAILED"
+            return JsonResponse(response, status=status.HTTP_404_NOT_FOUND)
+        body = list()
+        for user in users:
+            body.append({'id': user.id, "first_name": user.user.first_name, "last_name": user.user.last_name, "email": user.user.email, "department": user.department.name, "year": user.year.name, "section": user.section.name})
+        response["user"] = body
+        return JsonResponse(response, status=status.HTTP_200_OK)
 
 @csrf_exempt
 def login(request):
@@ -59,13 +96,14 @@ def login(request):
         if(user.count() > 0):
             response["password"] = user[0].check_password(user_data["password"])
             if(response["password"]):
+                response["user_id"] = user[0].id
                 response["message"] = "Password Matched"
             else:
                 response["message"] = "Password Mismatched"
         else:
             response["password"] = False
             response["message"] = "Invalid UserName"
-        return JsonResponse(response)
+        return JsonResponse(response, status=status.HTTP_200_OK)
 
 @csrf_exempt
 def unique_username(request):
@@ -75,4 +113,4 @@ def unique_username(request):
         count = User.objects.filter(username=json_data["roll_no"]).count()
         if(count > 0):
             response["unique"] = False
-        return JsonResponse(response)
+        return JsonResponse(response, status=status.HTTP_200_OK)
