9a3f9eecf3fed45dd84a6af2f3041f2bb39b4b89
harish988 <harishharish988@gmail.com>
2020-05-31 19:44:28 +0530

    Added real DB credentials

--- quiz/settings.py
+++ quiz/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'quiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'quiz',
+        'USER': 'mepcoquiz@mepcoquiz',
+        'PASSWORD': 'Mut8t69U',
+        'HOST': 'mepcoquiz.database.windows.net',
+        'PORT': '1433',
     }
 }
 
